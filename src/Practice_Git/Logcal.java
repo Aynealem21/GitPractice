2dc5f775e5ef28cf2d61262c81ce7c866088a1d2
Aynealem21 <72678341+Aynealem21@users.noreply.github.com>
2020-10-11 14:49:05 -0500

    Update Logcal.java

--- src/Practice_Git/Logcal.java
+++ src/Practice_Git/Logcal.java
@@ -4,5 +4,6 @@ public class Logcal {
     public static void main(String[] args) {
         System.out.println("what is the best benefit of this things");
         System.out.print("this strategy is improtant");
+        //I want to see this in intellij
     }
 }
