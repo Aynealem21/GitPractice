db5711d523b7d8c6548d203ac63b412487c5cbc3
MEHARY GEBREMARIAM <mehary.gebremariam@gmail.com>
2020-10-11 13:39:37 -0500

    git Practice

--- src/Practice_Git/Git_Practice.java
+++ src/Practice_Git/Git_Practice.java
@@ -1,4 +1,9 @@
 package Practice_Git;
 
 public class Git_Practice {
+    public static void main(String[] args) {
+        System.out.println("this is meant for the git Practice");
+        System.out.println("go");
+
+    }
 }
