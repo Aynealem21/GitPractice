d7ea282a979318832bc727916d425f524b12d296
MEHARY GEBREMARIAM <mehary.gebremariam@gmail.com>
2020-10-11 13:51:23 -0500

    git practice

--- src/Practice_Git/Git_Practice.java
+++ src/Practice_Git/Git_Practice.java
@@ -1,9 +1,16 @@
 package Practice_Git;
 
 public class Git_Practice {
+    static{
+        System.out.println("This is the static block, it is really goood");
+
+    }
     public static void main(String[] args) {
+        System.out.println("Mehary will offer first from batch20");
         System.out.println("this is meant for the git Practice");
         System.out.println("go");
+
+
 
     }
 }
