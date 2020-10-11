251197494091ed3ffed4f240db570cb682ab2232
Aynealem21 <72678341+Aynealem21@users.noreply.github.com>
2020-10-11 14:45:54 -0500

    Update Logcal.java
    updated

--- src/Practice_Git/Logcal.java
+++ src/Practice_Git/Logcal.java
@@ -3,5 +3,6 @@ package Practice_Git;
 public class Logcal {
     public static void main(String[] args) {
         System.out.println("what is the best benefit of this things");
+        System.out.print("this strategy is improtant");
     }
 }
